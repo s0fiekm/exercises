128f6c17b8fea6b4759a1e247ec8690c5cd56819
Sofie <sokj0001@kea.stud.dk>
2024-10-07 14:49:36 +0200

    done

--- profanity_class/index.js
+++ profanity_class/index.js
@@ -4,17 +4,27 @@ const curseWords = [
   { bad: "marquee", good: "just don't" },
 ];
 
+let theSentenceHasBeenTreated = false;
 let theText = document.querySelector("p").textContent;
 document.querySelector("button").addEventListener("click", replaceBadWords);
 
 function replaceBadWords() {
   //   console.log("Replace", theText);
-  curseWords.forEach((curseWord) => {
-    console.log((theText = theText.replaceAll(curseWord.bad, curseWord.good)));
-  });
-  console.log(theText);
+  if (theSentenceHasBeenTreated === false) {
+    curseWords.forEach((curseWord) => {
+      theText = theText.replaceAll(
+        curseWord.bad,
+        `<span>${curseWord.good}</span>`
+      );
+    });
+    console.log(theText);
+    //   document.querySelector("p").textContent = theText;
+    theSentenceHasBeenTreated = true;
+  } else {
+    document.querySelector("dialog").showModal();
+  }
 }
 
-function isItSafe() {
-  return false;
-}
+// function isItSafe() {
+//   return false;
+// }
